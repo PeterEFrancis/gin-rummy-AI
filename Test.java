a1856442596040466f57b8bd835753ac2715ac89
Taylor <nellta01@gettysburg.edu>
2020-05-18 11:10:13 -0400

    Added another line to print

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@
 
 public class Test {
   public static void main(String[] args) {
-    System.out.print("Hello, World!");
+    System.out.println("Hello, World!");
+    System.out.println("This is an edit to the Test file")
   }
 }
