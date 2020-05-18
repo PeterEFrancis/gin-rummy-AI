9d3f9ff251be54967e3aa510289e829345077862
Taylor <nellta01@gettysburg.edu>
2020-05-18 14:22:02 -0400

    Fixed the heckin semicolon

--- Test.java
+++ Test.java
@@ -1,12 +1,13 @@
 
 //hi i'm taylor
 //this is ryzeson
+import java.util.Random;
 
 public class Test {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println("This is an edit to the Test file");
-    System.out.println("Another line");
+    System.out.println("Another line")
     System.out.println(returnSomething(1,"hi",'d'));
   }
 
