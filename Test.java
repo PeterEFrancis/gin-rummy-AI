b624dff369275e339560126ce7723afe84a00a8f
Taylor <nellta01@gettysburg.edu>
2020-05-18 12:37:52 -0400

    Implemented advanced function processing to calculate response text

--- Test.java
+++ Test.java
@@ -1,17 +1,16 @@
 
-
 //hi i'm taylor
 //this is ryzeson
 
 public class Test {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
-<<<<<<< HEAD
     System.out.println("This is an edit to the Test file");
     System.out.println("Another line");
-=======
-    System.out.println("This is an edit to the Test file")
-    System.out.println("This is also an edit to the Test file")
->>>>>>> 07c0e9157d2adcd5f4210553160ba5fd601fa84e
+    System.out.println(returnSomething(1,"hi",'d'));
+  }
+
+  public static String returnSomething(int one, String word, char a) {
+    return "yeet";
   }
 }
